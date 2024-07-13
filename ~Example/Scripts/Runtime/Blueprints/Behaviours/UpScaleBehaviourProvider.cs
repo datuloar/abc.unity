b3831aa6d26e69fecb62c81db003ca094f4a8d51
Daniil Potapov <58138724+datuloar@users.noreply.github.com>
2024-07-14 03:55:13 +0500

    fix git resources

--- ~Example/Scripts/Runtime/Blueprints/Behaviours/UpScaleBehaviourProvider.cs
+++
@@ -1,9 +0,0 @@
-﻿using abc.unity.Core;
-
-namespace abc.unity.Example
-{
-    public class UpScaleBehaviourProvider : AbstractActorBehaviourProvider<UpScaleBehaviour>
-    {
-
-    }
-}
--- ~Example/Scripts/Runtime/Data/JumpData.cs
+++
@@ -1,9 +0,0 @@
-﻿using UnityEngine;
-
-namespace abc.unity.Example
-{
-    public class JumpData : MonoBehaviour, IJumpData
-    {
-       [field: SerializeField] public float Force { get; private set; }
-    }
-}
